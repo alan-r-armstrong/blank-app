694100f81ea2b0177289420092232375df6b9305
Alan <79943006+alan-r-armstrong@users.noreply.github.com>
2025-02-28 22:57:17 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,28 @@ from pages.page_1 import func_page_1
 from pages.page_2 import func_page_2
 from datetime import datetime
 
+def main():
+    st.sidebar.subheader('Page selection')
+    page_selection = st.sidebar.selectbox('Please select a page',['Main Page',
+    'Page 1','Page 2'])
+    pages_main = {
+        'Main Page': main_page,
+        'Page 1': run_page_1,
+        'Page 2': run_page_2
+    }
+
+    # Run selected page
+    pages_main[page_selection]()
+
+def main_page():
+    st.title('Main Page')
+def run_page_1():
+    func_page_1()
+def run_page_2():
+    func_page_2()
+if __name__ == '__main__':
+    main()
+
 st.set_page_config(
 
     page_title='Hello world',
@@ -26,27 +48,3 @@ while True:
         clock.empty()
         st.warning("Time's up!")
         break
-
-
-
-def main():
-    st.sidebar.subheader('Page selection')
-    page_selection = st.sidebar.selectbox('Please select a page',['Main Page',
-    'Page 1','Page 2'])
-    pages_main = {
-        'Main Page': main_page,
-        'Page 1': run_page_1,
-        'Page 2': run_page_2
-    }
-
-    # Run selected page
-    pages_main[page_selection]()
-
-def main_page():
-    st.title('Main Page')
-def run_page_1():
-    func_page_1()
-def run_page_2():
-    func_page_2()
-if __name__ == '__main__':
-    main()
